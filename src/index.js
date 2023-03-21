92b884e15f36092f2efbf5bea380ae5ef818842f
usefercas <124436934+usefercas@users.noreply.github.com>
2023-03-21 17:43:23 +0100

    ejercicio solucionado

--- src/index.js
+++ src/index.js
@@ -1,42 +1,89 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  // conseguimos el precio obteniendo la etiqueta span de la clase price que lo guarda
+  const price = product.querySelector(".price span").textContent;
+  // conseguimos la cantidad del input 
+  const quanty = product.querySelector('.quantity input').value;
+  console.log("Precio -> " + price);
+  console.log("quantity -> " + quanty);
+  // guardamos en totalPrice el precio total
+  const totalPrice = price * quanty;
+  console.log("Precio total de la fila -> " + totalPrice);
+  // insertamos en el span el con textContext el totalPrice para que se muestre en el html
+  product.querySelector(".subtotal span").textContent = totalPrice;
+  return totalPrice;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  console.log('invocando calculateAll');
+  // querySelectorAll devuelve todos los elementos anotados con la clase .product y los guarda en productosHtmlCollection
+  let productosHtmlCollection = document.querySelectorAll(".product");
+  let total = 0;
+  // casteamos la lista de productos para poder iterarla con un foreach
+  Array.from(productosHtmlCollection).forEach(producto => {
+    // imprimimos el producto que vamos a actualizar para poder debugear
+    console.log("actualizando producto -> " + producto.toString());
+    // total = total + updateSubtotal(producto);
+    // acumulamos en total los precios de los productos (puede haber varios items por producto)
+    total += updateSubtotal(producto);
+  });
 
-  // ITERATION 2
-  //... your code goes here
+  console.log("Precio total -> " + total);
+  // accedemos con document al span que esta dentro del h2 con id total-value y le insertamos textContext el total
+  // que habiamos calculado arriba
+  document.querySelector("#total-value span").textContent = total;
 
-  // ITERATION 3
-  //... your code goes here
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
-  const target = event.currentTarget;
-  console.log('The target in remove is:', target);
   //... your code goes here
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
-}
+  // obtengo el nombre por id del nuevo producto
+  const nameProduct = document.querySelector("#nombre-producto").value;
+  // obtengo el precio por id del nuevo producto
+  const price = document.querySelector("#price-new-product").value;
 
+  // creo una nueva fila para insertar
+  const newRow = document.createElement('tr');
+  // le pongo la clase para que tenga el mismo estilo y js que el resto
+  newRow.setAttribute('class', 'product');
+
+  // añade la nueva file al body
+  document.querySelector('tbody').appendChild(newRow);
+  // escribimos las columnas dentro del tr interpolando los valores que necesitamos
+  newRow.innerHTML = `
+  <td class="name">
+    <span>${nameProduct}</span>
+  </td>
+  <td class="price">$<span>${price}</span></td>
+  <td class="quantity">
+    <input type="number" value="0" min="0" placeholder="Quantity" />
+  </td>
+  <td class="subtotal">$<span>0</span></td>
+  <td class="action">
+    <button id="button" class="btn btn-remove">Remove</button>
+  </td>
+`
+}
+// usamos window para añadir un event listener 'load' que ejecuta la funciona anonima 
+// que le pasamos cuando se carga la ventana del navegador
 window.addEventListener('load', () => {
+  console.log('Load cargado');
+  // Conseguimos el boton y lo guardamos en una variable
+  // botonCalcularPrecio     //objeto  //function      //argumento
   const calculatePricesBtn = document.getElementById('calculate');
+  // añadimos un event listener al boton 'click' y le pasamos la funcion que hemos definido calculateAll
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  // crea elemento en lista
+  const createButton = document.querySelector("#create");
+  createButton.addEventListener("click", createProduct);
+
 });
